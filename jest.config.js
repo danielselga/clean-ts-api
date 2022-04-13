d914b0e9dd7508c553a23554356d55e972c609c4
Daniel Selga | Growyx <danielselga@gmail.com>
2022-04-13 02:50:33 -0300

    chore: add jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
