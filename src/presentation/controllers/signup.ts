d2bbad4cf47cfc7a7ad608b204134e9575e3b81f
Daniel Selga <danielselga@gmail.com>
2022-04-16 17:52:15 -0300

    refactor: make SignUpController conforms to a protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,11 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missingParamError'
 import { badRequest } from '../helpers/httpHelper'
+import { Controller } from '../protocols/controller'
 
-export class SignUpController {
+// Implements a class means we can tye the class as the interface.
+// This is a very good practice.
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
