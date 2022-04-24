7b810e164f688e060c98cfd01587f7ec88bef9bc
Daniel Selga <danielselga@gmail.com>
2022-04-24 01:57:17 -0300

    fix: fixing jest-mongodb config file

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: ['ts-jest', '@shelf/jest-mongodb'],
+  preset: '@shelf/jest-mongodb',
   testEnvironment: 'node',
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
