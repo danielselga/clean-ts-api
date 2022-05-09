8b2dfa76ac89849145ca5451176de530cfc851da
Daniel Selga <danielselga@gmail.com>
2022-05-09 01:18:39 -0300

    feat: add decorators layer

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,14 @@ import { EmailValidatorAdapter } from '../../utils/EmailValidatorAdapter'
 import { DbAddAccount } from '../../data/usecases/AddAccount/dbAddAccount'
 import { BcryptAdapter } from '../../infra/cryptography/bcryptAdapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/accountRepository/account'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return new LogControllerDecorator(signUpController)
 }
